f40547b427c12f363363b0a6627ea01d7aafa7d9
I Fan Wu <peterwu@Is-MacBook-Pro.local>
2019-10-04 22:55:06 -0500

    made the annotation file smaller

--- final_project/parse_data.R
+++ final_project/parse_data.R
@@ -1,10 +1,18 @@
-#render the .RData obj into a .txt
+# render the .RData obj(s) into .txt(s)
 currentDir=dirname(rstudioapi::getActiveDocumentContext()$path)
 
+
+## phenotpye data
 load(paste(currentDir,"Nich_Price_quantitative.RData",sep="/"))
-
 str(Nich_Price_quantitative)
-
 Nich_Price_quantitative=Nich_Price_quantitative[order(as.numeric(rownames(Nich_Price_quantitative))),]
-
 write.table(Nich_Price_quantitative,file=paste(currentDir,"Nich_Price_quantitative.csv",sep="/"),sep=",",quote=F,row.names = T,col.names = F)
+
+
+## annotations
+load(paste(currentDir,"annot_dummies.RData",sep="/"))
+str(annot_dummies)
+annot_dummies=ifelse(annot_dummies==T,1,0)
+write.table(annot_dummies,file=paste(currentDir,"annot_dummies.csv",sep="/"),sep=",",quote=F,row.names = T,col.names = T)
+
+
