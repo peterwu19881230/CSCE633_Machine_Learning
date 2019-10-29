c614cf672776a36ee465ddffac68c81b121c2b31
I Fan Wu <peterwu@Is-MacBook-Pro.local>
2019-10-28 20:47:18 -0500

    upload Fuhrer's

--- final_project/parse_data.R
+++ final_project/parse_data.R
@@ -8,6 +8,25 @@ str(Nich_Price_quantitative)
 Nich_Price_quantitative=Nich_Price_quantitative[order(as.numeric(rownames(Nich_Price_quantitative))),]
 write.table(Nich_Price_quantitative,file=paste(currentDir,"Nich_Price_quantitative.csv",sep="/"),sep=",",quote=F,row.names = T,col.names = F)
 
+load("Data/fuhrer_quantitative.RData")
+
+#I want to upload to github so I have to partition if I don't want to use git large file system
+fuhrer_quantitative_part1=fuhrer_quantitative[,1:1506]
+fuhrer_quantitative_part2=fuhrer_quantitative[,1507:3012]
+fuhrer_quantitative_part3=fuhrer_quantitative[,3013:4518]
+fuhrer_quantitative_part4=fuhrer_quantitative[,4519:6024]
+fuhrer_quantitative_part5=fuhrer_quantitative[,6025:7534]
+
+file_names=paste0("fuhrer_quantitative_part",1:5,".csv")
+files=list(fuhrer_quantitative_part1,fuhrer_quantitative_part2,fuhrer_quantitative_part3,fuhrer_quantitative_part4,fuhrer_quantitative_part5)
+
+for (i in seq(file_names)){
+  dat=files[[i]]
+  write.table(dat,file=paste(currentDir,file_names[i],sep="/"),sep=",",quote=F,row.names = F,col.names = F)
+}
+
+
+
 
 ## annotations
 load(paste(currentDir,"annot_dummies.RData",sep="/"))
